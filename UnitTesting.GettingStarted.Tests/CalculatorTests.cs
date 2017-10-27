412078112bd66f6173a8e87f8629bfd53bd6dd4a
Rabin Pun <rabinpun33@gmail.com>
2017-10-27 15:58:18 -0500

    calculator test , also with testcase

---
+++ UnitTesting.GettingStarted.Tests/CalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿
+using NUnit.Framework;
+
+namespace UnitTesting.GettingStarted.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_Always_ReturnsExpectedResult()  //[subject]_[scenario]_[result] i.e GrantLoan_WhenCreditLessthan500_ReturnsFalse
+        {
+            var systemUnderTest = new Calculator();
+            Assert.That(systemUnderTest.Add(1, 3), Is.EqualTo(4));
+        }
+
+        [TestCase(1,2)]
+        [TestCase(0,9)]
+        [TestCase(100003, 349029)]
+        public void Add_Always_ReturnsExpectedResult_WithTestCaseExample(int lhs, int rhs)
+        {
+            var systemUnderTest = new Calculator();
+            var expectedValue = lhs + rhs;
+            Assert.That(systemUnderTest.Add(lhs, rhs), Is.EqualTo(expectedValue));
+        }
+    }
+}
---
+++ UnitTesting.GettingStarted/Calculator.cs
@@ -0,0 +1,11 @@
+﻿
+namespace UnitTesting.GettingStarted
+{
+    public class Calculator
+    {
+        public int Add(int lhs, int rhs)
+        {
+            return lhs + rhs;
+        }
+    }
+}
