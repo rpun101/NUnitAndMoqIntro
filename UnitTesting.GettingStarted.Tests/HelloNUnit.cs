03f728b45d0f9c990efd703fca0dad4765c06ee7
Rabin Pun <rabinpun33@gmail.com>
2017-10-27 15:36:29 -0500

    first commit with first unit test

---
+++ UnitTesting.GettingStarted.Tests/HelloNUnit.cs
@@ -0,0 +1,19 @@
+﻿
+using NUnit.Framework;
+
+namespace UnitTesting.GettingStarted.Tests
+{
+    [TestFixture]
+    public class HelloNUnit
+    {
+
+        [Test]
+        public void TestHelloNUnit()  //method or class name does not need to have Test* in it, but attribute is required
+        {
+            //Assert.That({acutal value from operation}, {expected value});
+            Assert.That(true, Is.True);
+        }
+    }
+
+
+}
