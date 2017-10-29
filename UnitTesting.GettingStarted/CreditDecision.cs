af1f65a373e5216c772b4697a9e4edf8ffe22bd2
Rabin Pun <rabinpun33@gmail.com>
2017-10-29 00:14:11 -0500

    add CreditDecsion, show use of dependency injection and abstraction

---
+++ UnitTesting.GettingStarted/CreditDecision.cs
@@ -0,0 +1,59 @@
+﻿
+namespace UnitTesting.GettingStarted
+{
+    /*
+    public class CreditDecision
+    {
+        public string MakeCreditDecision(int creditScore)
+        {
+            var service = new CreditDecisionService(); || NEW creates dependency
+            return service.GetCreditDecision(creditScore);
+        }
+    }
+     * 
+     * Here NEW keyword creates the dependcy with CreditDecisionService Class!
+
+    */
+
+    /*
+     * 
+     * public class CreditDecision{
+
+    CreditDecisionService creditDecisionService;
+    public CreditDecision(CreditDecisionService creditDecisionService)
+    {
+        this.creditDecisionService = creditDecisionService;
+    }
+
+    public string MakeCreditDecision(int creditScore){
+       return creditDecisionService.GetCreditDecision(creditScore);
+    }
+     * 
+     * 
+     * 
+     * Here we moved our dependency to the constructor. good we said we need CreditDecisionService
+     * when we create CreditDecision but STILL this requires the concrete.
+     * We need to interface or abstract class for the abstraction.
+}
+     * */
+
+    public interface ICreditDecisionService
+    {
+        string GetCreditDecision(int creditScore);
+    }
+
+    public class CreditDecision
+    {
+        ICreditDecisionService creditDecisionService;
+
+        public CreditDecision(ICreditDecisionService creditDecisionService)
+        {
+            this.creditDecisionService = creditDecisionService;
+        }
+
+        public string MakeCreditDecision(int creditScore)
+        {
+            return creditDecisionService.GetCreditDecision(creditScore);
+        }
+    }
+}
