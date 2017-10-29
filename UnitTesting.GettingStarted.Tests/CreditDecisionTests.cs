e8f6e41163316aefa2a21ff8b5808e0c27fead00
Rabin Pun <rabinpun33@gmail.com>
2017-10-29 00:51:14 -0500

    add MockService for testing-end of part 1

---
+++ UnitTesting.GettingStarted.Tests/CreditDecisionTests.cs
@@ -0,0 +1,37 @@
+﻿
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTesting.GettingStarted.Tests
+{
+    [TestFixture]
+    public class CreditDecisionTests
+    {
+        //mock instance for dependency
+        Mock<ICreditDecisionService> mockCreditDecisionService;
+
+        CreditDecision systemUnderTest;
+
+        [TestCase(100, "Declined")]
+        [TestCase(549, "Declined")]
+        [TestCase(550, "Maybe")]
+        [TestCase(674, "Maybe")]
+        [TestCase(675, "We look forward to doing business with you!")]
+        public void MakeCreditDecision_Always_ReturnsExpectedResult(int creditScore, string expectedResult)
+        {
+
+            //Arrange
+            //creating and configuring the mock 
+            mockCreditDecisionService = new Mock<ICreditDecisionService>(MockBehavior.Strict);
+            mockCreditDecisionService.Setup(p => p.GetCreditDecision(creditScore)).Returns(expectedResult);
+            //[above line: when mock service is called and with 'creditScore', returns 'expectedResult']
+
+            systemUnderTest = new CreditDecision(mockCreditDecisionService.Object);
+            var result = systemUnderTest.MakeCreditDecision(creditScore);
+
+            Assert.That(result, Is.EqualTo(expectedResult));
+
+            mockCreditDecisionService.VerifyAll();
+        }
+    }
+}
